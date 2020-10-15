56358e38080ca3f3f925cf054ff97e97671261b5
오지수 <wltn0213@gmail.com>
2020-10-15 16:48:37 +0900

    S03P31A105-30 | material design 적용

--- front/src/App.vue
+++ front/src/App.vue
@@ -36,7 +36,22 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
+    
+    <div style="margin-top: 100px;">
+      <v-btn color="primary">primary</v-btn>
+      <v-btn color="accent">accent</v-btn>
+      <v-btn color="tertiary">tertiary</v-btn>
+      <v-btn color="secondary">secondary</v-btn>
+    </div>
+      <div style="magin-left: 50px;">
+      <div class="text-h1">h1</div>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+      <h5>h5</h5>
+      <h6>h6</h6>
+    </div>
+  
     <v-main>
       <HelloWorld/>
     </v-main>
