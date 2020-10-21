a6674bfd5eb1fd015f881b188071559046e2a312
seoroda <seoroda@gmail.com>
2020-10-22 02:59:31 +0900

    S03P31A105-14 | 게시글 목록 조회

--- back/IoTBackend/src/main/java/com/ssafy/IoTBackend/service/PostService.java
+++ back/IoTBackend/src/main/java/com/ssafy/IoTBackend/service/PostService.java
@@ -1,9 +1,13 @@
 package com.ssafy.IoTBackend.service;
 
+import java.util.List;
+
 import com.ssafy.IoTBackend.model.Post;
 
 public interface PostService {
 
+	public List<Post> selectPosts(String user_id) throws Exception;
+	
 	public void insertPost(Post postDto) throws Exception;
 
 }
