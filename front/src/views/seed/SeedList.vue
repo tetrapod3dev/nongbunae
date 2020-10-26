cc83382da002ff3519d5553f8c6c222834ea5ffa
Taerok <sdf7575@naver.com>
2020-10-26 15:36:10 +0900

    ADD seed view

---
+++ front/src/views/seed/SeedList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div style="background-color: #fff8f3">
+    <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <div class="pl-3 text-h6">새싹 목록</div>
+        </v-col>
+
+        <v-col v-for="(item, index) in items" :key="index" cols="12">
+          <CoreInfoCard
+            :subtitle="item.subtitle"
+            :title="item.title"
+            :btn="item.btn"
+            :src="item.src"
+            @click.native="$set(sheet, index - 1, true)"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <CoreBottomNav />
+  </div>
+</template>
+
+<script>
+import CoreBottomNav from "@/components/core/BottomNav.vue";
+import CoreInfoCard from "@/components/core/InfoCard.vue";
+
+export default {
+  name: "SeedList",
+  components: {
+    CoreBottomNav,
+    CoreInfoCard,
+  },
+  data() {},
+};
+</script>
+
+<style scoped>
+</style>
