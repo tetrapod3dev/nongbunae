8b0491dac25e396854a3a7247f3a07bd2b43bdb0
오지수 <wltn0213@gmail.com>
2020-10-28 14:23:02 +0900

    S03P31A105-63 | 카카오 로그인 연결

--- front/src/main.js
+++ front/src/main.js
@@ -4,11 +4,13 @@ import vuetify from "./plugins/vuetify";
 import router from "./router";
 import Vuex from 'vuex';
 import store from "./store";
+import axios from 'axios';
 
 Vue.use(Vuex)
+Vue.use(axios)
 
 Vue.config.productionTip = false;
-window.Kakao.init(process.env.VUE_APP_KAKAO_APP_KEY);
+window.Kakao.init('ccef2764def2c00faa00ebd5e4583007');
 new Vue({
   vuetify,
   router,
