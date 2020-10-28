5008b5af78ba5e797d16383b3514fe42e757d5dc
오지수 <wltn0213@gmail.com>
2020-10-28 11:57:08 +0900

    S03P31A105-63 | 카카오 로그인 vuex 추가

--- front/src/main.js
+++ front/src/main.js
@@ -2,11 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import Vuex from 'vuex';
+import store from "./store";
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false;
 window.Kakao.init(process.env.VUE_APP_KAKAO_APP_KEY);
 new Vue({
   vuetify,
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
