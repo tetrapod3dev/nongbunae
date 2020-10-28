297f6783db05ae78ec132732c90dd9aeb0e310ad
seoroda <seoroda@gmail.com>
2020-10-28 11:49:56 +0900

    S03P31A105-28 | 재배 일정 생성

--- back/IoTBackend/src/main/java/com/ssafy/IoTBackend/service/CalendarService.java
+++ back/IoTBackend/src/main/java/com/ssafy/IoTBackend/service/CalendarService.java
@@ -3,8 +3,11 @@ package com.ssafy.IoTBackend.service;
 import java.util.List;
 
 import com.ssafy.IoTBackend.model.calendar.Calendar;
+import com.ssafy.IoTBackend.model.calendar.CreateCalendarRequestDTO;
 
 public interface CalendarService {
+	
+	public void insertCalendar(CreateCalendarRequestDTO calendarDTO) throws Exception;
 
 	public List<Calendar> selectCalendar(String user_id) throws Exception;
 
