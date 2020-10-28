fbc9e0a0c6627de9b6521af4299848d8d05ce7b1
오지수 <wltn0213@gmail.com>
2020-10-28 18:50:37 +0900

    S03P31A105-88 | 캘린더 구현중

---
+++ front/src/components/plant/PlantWeekCalendar.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="d-flex align-center">
+    
+  <v-btn fab text small @click="prev"><v-icon small>mdi-chevron-left</v-icon></v-btn>
+  <v-calendar-weekly ref="calendarWeek"/>
+  <v-btn fab text small @click="next"><v-icon small>mdi-chevron-right</v-icon></v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            events: [],
+            today: new Date()
+        }
+    },
+    methods: {
+      prev () {
+        this.$refs.calendarWeek.start = this.today.setDate(this.today.getDate() - 7)
+      },
+      next () {
+        this.$refs.calendarWeek.start = this.today.setDate(this.today.getDate() + 7)
+      },
+      
+    },
+    mounted() {
+
+    }
+    
+
+}
+</script>
+
+<style>
+
+</style>
