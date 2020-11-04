4f638e7394e0bdb6df7e1da78d8285bc2de3d4d1
박태록 <tetrapod3dev@gmail.com>
2020-11-05 04:29:56 +0900

    UPDATE plant main header info layout

--- front/src/views/plant/PlantMain.vue
+++ front/src/views/plant/PlantMain.vue
@@ -10,33 +10,53 @@
     }"
   >
     <v-row class="d-flex align-self-start">
-      <v-col cols="12" class="pb-0">
-        <v-img src="@/assets/icon/info.svg" contain>
-          <v-row>
-            <v-col cols="2" />
-            <v-col cols="2" class="text-right">
-              <div class="nbn--info nbn--info-subtitle text-left">온도</div>
-              <div class="nbn--info">100˚C</div>
-            </v-col>
-            <v-col cols="2" />
-            <v-col cols="2" class="pl-0">
-              <div class="nbn--info nbn--info-subtitle text-left">습도</div>
-              <div class="nbn--info">100%</div>
-            </v-col>
-            <v-col cols="1"> </v-col>
-            <v-col cols="3" class="text-left">
-              <div class="nbn--info nbn--info-subtitle text-left">자동급수</div>
-              <div class="nbn--info">13:06</div>
-            </v-col>
-          </v-row>
-        </v-img>
+      <v-col cols="12" class="py-0 px-3">
+        <v-row
+          no-gutters
+          style="
+            border-radius: 30px;
+            background-color: rgba(255, 255, 255, 0.5);
+          "
+        >
+          <v-col cols="2" class="d-flex align-center">
+            <v-img src="@/assets/icon/thermometer.svg" height="36px" contain />
+          </v-col>
+          <v-col cols="2">
+            <div class="nbn--info nbn--info-subtitle text-left">온도</div>
+            <div class="nbn--info">100˚C</div>
+          </v-col>
+          <v-col cols="2" class="d-flex align-center">
+            <v-img src="@/assets/icon/hygrometer.svg" height="36px" contain />
+          </v-col>
+          <v-col cols="2">
+            <div class="nbn--info nbn--info-subtitle text-left">습도</div>
+            <div class="nbn--info">100%</div>
+          </v-col>
+          <v-col cols="2" class="d-flex align-center">
+            <v-img src="@/assets/icon/watering.svg" height="36px" contain />
+          </v-col>
+          <v-col cols="2">
+            <div class="nbn--info nbn--info-subtitle text-left">자동급수</div>
+            <div class="nbn--info">13:06</div>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
     <v-row class="d-flex align-self-start justify-end">
       <v-col cols="2" class="py-0">
-        <v-img src="@/assets/icon/paper.svg" class="mb-2" contain />
+        <v-img
+          src="@/assets/icon/paper.svg"
+          height="36px"
+          class="mb-2"
+          contain
+        />
         <router-link :to="{ name: 'PlantChoice' }">
-          <v-img src="@/assets/icon/change.svg" class="mb-2" contain />
+          <v-img
+            src="@/assets/icon/change.svg"
+            height="36px"
+            class="mb-2"
+            contain
+          />
         </router-link>
       </v-col>
     </v-row>
@@ -48,14 +68,21 @@
       <v-col cols="2"></v-col>
     </v-row>
     <v-row>
-      <v-col cols="1"></v-col>
-      <v-col cols="9">
-        <v-progress-linear
-          value="60"
-          height="25"
+      <div
+        class="pa-0 ml-auto mr-4"
+        style="border-radius: 50%; background-color: rgba(255, 255, 255, 0.5)"
+      >
+        <v-progress-circular
+          :rotate="-90"
+          :size="80"
+          :width="20"
+          :value="plantInfo.percent"
           :color="itemProgressColor[potColor]"
-        ></v-progress-linear>
-      </v-col>
+        >
+          <span class="nbn--progress">{{ plantInfo.percent }}</span>
+          <span class="nbn--info">%</span>
+        </v-progress-circular>
+      </div>
     </v-row>
   </v-container>
 </template>
@@ -126,9 +153,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nbn--progress {
+  color: #5b3016;
+  font-size: 24px;
+  font-weight: 900;
+  font-family: "Jua", sans-serif;
+}
+
 .nbn--info {
   color: #5b3016;
-  font-size: 20px;
+  font-size: 18px;
   font-family: "Jua", sans-serif;
 
   &-subtitle {
