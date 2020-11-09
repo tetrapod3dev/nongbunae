393a94cee1e9349f8c066a2d86f608958d85a906
Taerok <sdf7575@naver.com>
2020-11-09 11:52:18 +0900

    UPDATE tutorial

--- front/src/views/plant/Tutorial.vue
+++ front/src/views/plant/Tutorial.vue
@@ -1,25 +1,26 @@
 <template>
-  <v-container fluid class="fill-height">
-    <v-row>
+  <v-container
+    fluid
+    class="fill-height"
+    :style="{ backgroundColor: '#dddddd' }"
+  >
+    <v-row no-gutters>
       <v-col cols="4" class="mx-auto">
         <span class="text-h5 align-self-center pb-5">튜토리얼</span>
       </v-col>
     </v-row>
     <v-carousel
-      height="700px"
       :hide-delimiter-background="true"
       :show-arrows="false"
       :continuous="false"
       delimiter-icon="mdi-minus"
+      light
     >
       <v-carousel-item>
         <TutorialCard :e1="1" />
       </v-carousel-item>
       <v-carousel-item>
         <TutorialCard :e1="2" />
-      </v-carousel-item>
-      <v-carousel-item>
-        <TutorialCard :e1="3" />
       </v-carousel-item>
     </v-carousel>
   </v-container>
