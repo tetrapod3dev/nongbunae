3cd199911b95f53958eee9337d6f947af2cc5ba1
Taerok <sdf7575@naver.com>
2020-11-12 22:10:02 +0900

    UPDATE totuiral step and skip button, refactory tuto card

--- front/src/components/plant/TutorialCard.vue
+++ front/src/components/plant/TutorialCard.vue
@@ -1,32 +1,40 @@
 <template>
   <div>
-    <v-card v-if="e1 == 1" class="rounded-lg">
+    <v-card class="rounded-lg">
       <v-img
-        :src="require('@/assets/plant/tutorial/튜토애니메이션1.gif')"
+        :src="require(`@/assets/plant/tutorial/튜토애니메이션${el}.gif`)"
         height="360px"
         contain
       />
-      <v-card-title> 씨앗 뿌리기 </v-card-title>
+      <v-card-title> {{ items[el].title }} </v-card-title>
       <v-card-subtitle>
-        재배할 씨앗을 수경 재배기에 골고루 뿌리세요.
+        {{ items[el].subtitle }}
       </v-card-subtitle>
-    </v-card>
-
-    <v-card v-if="e1 == 2" class="rounded-lg">
-      <v-img
-        :src="require('@/assets/plant/tutorial/튜토애니메이션2.gif')"
-        height="360px"
-        contain
-      />
-      <v-card-title> 물 넣기 </v-card-title>
-      <v-card-subtitle> 물 주입구를 통해 물을 넣어주세요 </v-card-subtitle>
     </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["e1"],
+  props: ["el"],
+  data() {
+    return {
+      items: [
+        {
+          title: "씨앗 뿌리기",
+          subtitle: "재배할 씨앗을 수경 재배기에 골고루 뿌리세요.",
+        },
+        {
+          title: "물 넣기",
+          subtitle: "물 주입구를 통해 물을 넣어주세요.",
+        },
+        {
+          title: "수확하기",
+          subtitle: "이제 수확할 때까지 기다리면 돼요!",
+        },
+      ],
+    };
+  },
 };
 </script>
 
