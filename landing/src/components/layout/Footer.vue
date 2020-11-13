75879f776d528c2a60c9e1ac19e49d6047d5b392
박지은 <wong_303@naver.com>
2020-11-13 16:41:04 +0900

    landing page

---
+++ landing/src/components/layout/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-footer
+    class="justify-center"
+    color="#292929"
+    height=""
+  >
+  <v-row class="font-weight-light grey--text text--lighten-1 text-center">
+    <v-col cols="3">
+      <span style="font-weight:bold">INFORMATION</span>
+      <v-spacer style="padding-bottom:10px"></v-spacer>
+       (주)농부네텃밭👨‍🌾
+    </v-col>
+
+    <v-col cols="3">
+      <span style="font-weight:bold">LOCATION</span>
+      <v-spacer style="padding-bottom:10px"></v-spacer>
+      서울 강남구 테헤란로 212(역삼동)<br>
+      멀티캠퍼스 역삼 12층
+    </v-col>
+
+    <v-col cols="3">
+      <span style="font-weight:bold">CONTACT</span>
+      <v-spacer style="padding-bottom:10px"></v-spacer>
+      Tel  02 3429 5114<br>
+      Fax  02 3429 5114
+    </v-col>
+
+    <v-col cols="3">
+      <span style="font-weight:bold">MEMBER</span>
+      <v-spacer style="padding-bottom:10px"></v-spacer>
+        <v-avatar  color="primary" size="45"  class="mr-3" tile> 
+          <v-img   src="@/assets/main/member/j.png"></v-img>
+        </v-avatar>
+        <v-avatar  color="primary" size="45"  class="mr-3" tile> 
+          <v-img   src="@/assets/main/member/p.png" ></v-img>
+        </v-avatar>
+        <v-avatar  color="primary" size="45"  class="mr-3" tile> 
+          <v-img   src="@/assets/main/member/p2.png" ></v-img>
+        </v-avatar>
+        <v-avatar  color="primary" size="45"  class="mr-3" tile> 
+          <v-img   src="@/assets/main/member/s.png" ></v-img>
+        </v-avatar>
+        <v-avatar  color="primary" size="45"  class="mr-3" tile> 
+          <v-img   src="@/assets/main/member/o.png" ></v-img>
+        </v-avatar>
+    </v-col>
+  </v-row> 
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+
+</style>
---
+++ landing/src/components/main/MainTutorialSection.vue
@@ -0,0 +1,74 @@
+<template>
+     <section
+        id="features"
+        class=" lighten-3"
+      >
+        <div class="py-12"></div>
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3" >‘농부네 텃밭’ 사용방법</h2>
+
+          <v-responsive
+            class="mx-auto mb-12"
+            width="56"
+          >
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-card class="py-12 px-4" color="grey lighten-5" flat   >
+                <v-card-title class="justify-center font-weight-black text-uppercase">1.씨앗을 심어주세요</v-card-title>
+                <v-theme-provider dark >
+                  <div>
+                    <v-img src="@/assets/main/tuto/tuto1.gif" height="250px"></v-img>
+                  </div>
+                </v-theme-provider>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-card class="py-12 px-4" color="grey lighten-5" flat>
+                <v-card-title class="justify-center font-weight-black text-uppercase">2.물을 넣어주세요</v-card-title>
+                <v-theme-provider dark>
+                  <div>
+                    <v-img src="@/assets/main/tuto/tuto2.gif" height="250px"></v-img>
+                  </div>
+                </v-theme-provider>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-card class="py-12 px-4" color="grey lighten-5" flat>
+                <v-card-title class="justify-center font-weight-black text-uppercase">3.수확을 확인하세요</v-card-title>
+                <v-theme-provider dark>
+                  <div>
+                    <v-img src="@/assets/main/tuto/tuto3.gif" height="250px"></v-img>
+                  </div>
+                </v-theme-provider>
+              </v-card>
+            </v-col>
+
+          </v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+</template>
+
+
+<script>
+export default {
+  name: "MainTutorialSection",
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>  
