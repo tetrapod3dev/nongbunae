2f27ef374f1fa071464780a7bfcc84287da1a9ff
seoroda <seoroda@gmail.com>
2020-11-15 14:50:56 +0900

    재배 작물 선택시 생성된 choice_id 반환하는 것으로 수정

--- back/IoTBackend/src/main/java/com/ssafy/IoTBackend/controller/ChoiceController.java
+++ back/IoTBackend/src/main/java/com/ssafy/IoTBackend/controller/ChoiceController.java
@@ -35,7 +35,7 @@ public class ChoiceController {
 	private UserService userService;
 	
 	@PostMapping
-	@ApiOperation(value = "재배할 작물 선택", notes = "성공시 'success' 실패시 'fail' 반환")
+	@ApiOperation(value = "재배할 작물 선택", notes = "성공시 생성된 choice_id 반환, 실패시 'fail' 반환")
 	public ResponseEntity<String> startPlantChoice(Authentication authentication, @RequestParam String plant_id) {
 		String userId = authentication.getPrincipal().toString();
 		try {
@@ -45,9 +45,9 @@ public class ChoiceController {
 				choice.setUser_id(userId);
 				choice.setPlant_id(plant_id);
 				
-				int result = choiceService.insertChoice(choice);
-				if(result == 0) return new ResponseEntity<>(FAIL, HttpStatus.NOT_FOUND);
-				else return new ResponseEntity<>(SUCCESS, HttpStatus.CREATED);
+				String new_choice_id = choiceService.insertChoice(choice);
+				if(new_choice_id.equals(null)) return new ResponseEntity<>(FAIL, HttpStatus.NOT_FOUND);
+				else return new ResponseEntity<>(new_choice_id, HttpStatus.CREATED);
 			}else {
 				return new ResponseEntity<>("유효하지 않은 인증 토큰입니다.", HttpStatus.FORBIDDEN);
 			}
