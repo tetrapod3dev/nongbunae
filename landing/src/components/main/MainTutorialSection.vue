652bcf4833f71303d4c7fd449b8493fd3ce82727
박지은 <wong_303@naver.com>
2020-11-15 17:41:21 +0900

    S03P31A105-136 | 랜딩페이지

--- landing/src/components/main/MainTutorialSection.vue
+++ landing/src/components/main/MainTutorialSection.vue
@@ -1,12 +1,15 @@
 <template>
      <section
         id="features"
-        class=" lighten-3"
+        class=" lighten-3 grey"
       >
         <div class="py-12"></div>
 
         <v-container class="text-center">
-          <h2 class="display-2 font-weight-bold mb-3" >‘농부네 텃밭’ 사용방법</h2>
+          <h2 class="display-3  mb-3" >
+            <span class="font-weight-bold">이렇게</span> 
+            사용하세요
+          </h2>
 
           <v-responsive
             class="mx-auto mb-12"
@@ -20,32 +23,38 @@
           <v-row>
             <v-col cols="12" md="4">
               <v-card class="py-12 px-4" color="grey lighten-5" flat   >
-                <v-card-title class="justify-center font-weight-black text-uppercase">1.씨앗을 심어주세요</v-card-title>
+                <v-card-title class="justify-center font-weight-black text-uppercase">
+                  <span  class="step">STEP1. </span> 씨앗을 심어주세요</v-card-title>
+                <v-theme-provider dark>
+                  <div >
+                    <v-img src="@/assets/main/tuto/tuto1.gif" height="380" contain></v-img>
+                  </div>
+                </v-theme-provider>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-card class="py-12 px-4" color="grey lighten-5" flat >
+                <v-card-title class="justify-center font-weight-black text-uppercase">
+                  <span  class="step">STEP2. </span> 물을 넣어주세요</v-card-title>
+                <v-theme-provider dark  >
+                  <div >
+                    <v-img src="@/assets/main/tuto/tuto2.gif" height="380" contain ></v-img>
+                  </div>
+                </v-theme-provider>
+                
+              </v-card>
+            </v-col>
+
+            <v-col cols="12" md="4" >
+              <!-- style="overflow-y: auto; height:500px" -->
+              <v-card class="py-12 px-4" color="grey lighten-5" flat >
+                <v-card-title class="justify-center font-weight-black text-uppercase">
+                  <span style="" class="step">STEP3. </span> 수확을 확인하세요
+                </v-card-title>
                 <v-theme-provider dark >
                   <div>
-                    <v-img src="@/assets/main/tuto/tuto1.gif" height="250px"></v-img>
-                  </div>
-                </v-theme-provider>
-              </v-card>
-            </v-col>
-
-            <v-col cols="12" md="4">
-              <v-card class="py-12 px-4" color="grey lighten-5" flat>
-                <v-card-title class="justify-center font-weight-black text-uppercase">2.물을 넣어주세요</v-card-title>
-                <v-theme-provider dark>
-                  <div>
-                    <v-img src="@/assets/main/tuto/tuto2.gif" height="250px"></v-img>
-                  </div>
-                </v-theme-provider>
-              </v-card>
-            </v-col>
-
-            <v-col cols="12" md="4">
-              <v-card class="py-12 px-4" color="grey lighten-5" flat>
-                <v-card-title class="justify-center font-weight-black text-uppercase">3.수확을 확인하세요</v-card-title>
-                <v-theme-provider dark>
-                  <div>
-                    <v-img src="@/assets/main/tuto/tuto3.gif" height="250px"></v-img>
+                    <v-img src="@/assets/main/tuto/tuto3.gif" height="380" contain></v-img>
                   </div>
                 </v-theme-provider>
               </v-card>
@@ -69,6 +78,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.step{
+  color:#9ACA7F;font-size:1.8rem;padding-right:10px;
+}
 </style>  
