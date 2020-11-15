6dacd947d182e3c638a1babb6e8d5de89125868c
오지수 <wltn0213@gmail.com>
2020-11-15 23:29:12 +0900

    오류 해결

--- front/src/components/core/BottomNav.vue
+++ front/src/components/core/BottomNav.vue
@@ -7,22 +7,23 @@
     color="primary"
     active-class="nbn--nav-active"
   >
-    <v-btn value="home" :to="{ name: 'PlantMain' }">
+    <v-btn value="home" :to="{ name: 'PlantMain' }" style="height: 100%;">
       <v-icon>$vuetify.icons.home</v-icon>
     </v-btn>
 
-    <v-btn value="diary" :to="{ name: 'PlantCalendar2' }">
+    <v-btn value="diary" :to="{ name: 'PlantCalendar2' }" style="height: 100%;">
       <v-icon>$vuetify.icons.calendar</v-icon>
     </v-btn>
 
     <v-btn
       value="faq"
       href="https://frogue.danbee.ai/?chatbot_id=8ac8ca73-ec86-4dd1-ba66-388919215cf5"
+      style="height: 100%;"
     >
       <v-icon>$vuetify.icons.faq</v-icon>
     </v-btn>
 
-    <v-btn value="account" :to="{ name: 'Mypage' }">
+    <v-btn value="account" :to="{ name: 'Mypage' }" style="height: 100%;">
       <v-icon>mdi-dots-horizontal</v-icon>
     </v-btn>
   </v-bottom-navigation>
