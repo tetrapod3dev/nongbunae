689b0155b7c79ce121a2abf60e3e6b290df69a48
jinpig2127 <jinpig2127@naver.com>
2020-11-15 15:14:14 +0900

    S03P31A105-121 | iot-actions camera 수정

--- back/RaspberryPi_WebsocketServer/websocketServer.py
+++ back/RaspberryPi_WebsocketServer/websocketServer.py
@@ -4,7 +4,32 @@ import websockets
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import pymysql
+import Adafruit_DHT as dht
+class Database():
+    def __init__(self):
+        self.db = pymysql.connect(host='3.35.131.33',
+                                  user='root',
+                                  password='ssafy',
+                                  db='ssafy_iot',
+                                  charset='utf8')
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
 
+    def execute(self, query, args={}):
+        self.cursor.execute(query, args)
+
+    def executeOne(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchone()
+        return row
+
+    def executeAll(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchall()
+        return row
+
+    def commit(self):
+        self.db.commit()
 
     # call back for websockets.serve(accept,
 async def accept(websocket, path):
@@ -42,13 +67,37 @@ async def accept(websocket, path):
 
                 GPIO.cleanup()
             elif action == "camera":
-                subprocess.call('sh ../imageTransfer.sh', shell=True)
+                img = subprocess.check_output('sh ./cycle2_imageTransfer.sh', shell=True)
+                img = str(img)
+                imgName = img.split("/")[4].split(".")
+                print("imgNmae is")
+                print(imgName[0])
+
+                # DB input
+                db_class = Database()
+                sql = "select pc.user_id as u, pc.plant_id p \
+                                from plant_choice as pc \
+                                where pc.choice_id =" + choice_id
+                row = db_class.executeAll(sql)
+                user_id = str(row[0]['u'])
+                plant_id = str(row[0]['p'])
+                
+                hum, temp = dht.read_retry(dht.DHT11, 4)
+
+                sql = "INSERT INTO raspberry(rb_temperature, rb_humidity, choice_id, user_id, plant_id, rb_img ) \
+                                                           VALUES(%s, %s, %s, \'%s\', %s, \'%s\')" % (
+                    temp, hum, choice_id, user_id, plant_id, str(imgName[0]))
+                db_class.execute(sql)
+                db_class.commit()
+
+
 
             elif action == "start":
                 print("Growing START")
                 print("choice_id", choice_id)
 
                 subprocess.call('python3 cycle1_watering.py ' + choice_id + "&" , shell=True)
+
 
 
             elif action == "stop":
@@ -71,4 +120,6 @@ websoc_svr = websockets.serve(accept, "192.168.219.110", 3000)
     # waiting
 asyncio.get_event_loop().run_until_complete(websoc_svr)
 asyncio.get_event_loop().run_forever()
+
+
 
