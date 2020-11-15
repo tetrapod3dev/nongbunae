99e2c2c8d27570b2c51835718255504877a1bde8
박지은 <wong_303@naver.com>
2020-11-15 23:36:58 +0900

    랜딩페이지

---
+++ front/src/components/layout/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-app-bar 
+      app
+      color="white"
+      height="100"
+    >
+      <v-avatar
+        class="mr-3"
+        color="grey lighten-5"
+        size="70"
+      >
+        <v-img
+          contain
+          max-height="100%"
+          src="@/assets/logo2.png" 
+        ></v-img>
+      </v-avatar>
+
+      <v-toolbar-title class="font-weight-black headline">
+        농부네 텃밭
+      </v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+}
+</script>
+
+<style scoped>
+
+</style>
