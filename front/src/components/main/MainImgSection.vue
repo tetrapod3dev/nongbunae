93d2b04154834a2e2cfce366264eef784da337a5
박태록 <tetrapod3dev@gmail.com>
2020-11-18 10:27:44 +0900

    UPDATE title and button in mainImgSection.vue

--- front/src/components/main/MainImgSection.vue
+++ front/src/components/main/MainImgSection.vue
@@ -1,23 +1,16 @@
 <template>
-  <section id="hero">   
+  <section id="hero">
     <v-row no-gutters>
       <v-img
-        :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+        :height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
         src="@/assets/main/h6.png"
+        position="top"
       >
         <v-theme-provider dark>
-          <v-container style="margin-top:60px">
+          <v-container fluid class="fill-height">
             <!-- fill-height -->
-            <v-row
-              align="center"
-              class="white--text mx-auto"
-              justify="center"
-            >
-              <v-col
-                class="white--text text-center"
-                cols="12"
-                tag="h1"
-              >
+            <v-row align="center" class="white--text mx-auto" justify="center">
+              <v-col class="white--text text-center" cols="12" tag="h1">
                 <!-- <span
                 :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
                   class="font-weight-light"
@@ -30,15 +23,19 @@
                 -->
                 <span
                   class="font-weight-black cafep"
-                  :class="[$vuetify.breakpoint.smAndDown ? 'f-size80': 'f-size130']"
-                  
+                  :class="[
+                    $vuetify.breakpoint.smAndDown ? 'f-size80' : 'f-size130',
+                  ]"
                 >
-                  <span class="h-bold1">농부네</span> <span class="f-green h-bold2" >텃밭</span>
+                  <span class="h-bold1">농부네</span>
+                  <span class="f-green h-bold2">텃밭</span>
                 </span>
               </v-col>
-
-              <v-btn 
-                class="align-self-end"
+            </v-row>
+            <v-row></v-row>
+            <v-row>
+              <v-btn
+                class="align-self-end mx-auto"
                 fab
                 color="#80AA6B"
                 @click="$vuetify.goTo('#about-me')"
@@ -57,38 +54,38 @@
 <script>
 export default {
   name: "MainImgSection",
-}
+};
 </script>
 
 <style scoped>
-.f-size130{
-  font-size:130px;
+.f-size130 {
+  font-size: 130px;
 }
 
-.f-size80{
-  font-size:75px;
+.f-size80 {
+  font-size: 75px;
 }
 
-.nanum{
-  font-family: 'Nanum Pen Script', cursive;
+.nanum {
+  font-family: "Nanum Pen Script", cursive;
 }
 
-.f-green{
-  color:#93c130;
+.f-green {
+  color: #93c130;
 }
 
-.nbp{
-  font-family: NanumBarunpen; 
+.nbp {
+  font-family: NanumBarunpen;
 }
 
-.cafep{
-  font-family: 'Cafe24Oneprettynight';
+.cafep {
+  font-family: "Cafe24Oneprettynight";
 }
-.h-bold1{
+.h-bold1 {
   text-shadow: -1px 0 #fff, 0 1px#fff, 1px 0 #fff, 0 -1px #fff;
 }
 
-.h-bold2{
+.h-bold2 {
   text-shadow: -1px 0 #93c130, 0 1px#93c130, 1px 0 #93c130, 0 -1px #93c130;
 }
 </style>  
